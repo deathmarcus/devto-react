3f97f333b1f3740b6fa23575fdeb8f32cb5a9123
Marco Castañeda <marco.dcc@gmail.com>
2022-09-21 14:48:58 -0500

    WIP Feat: Navbar

--- src/components/Nav/Navbar.js
+++ src/components/Nav/Navbar.js
@@ -1,7 +1,9 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../contexts/UserContext";
-import NavbarComponents from "./NavbarComponents";
+import Offcanvas from "./Offcanvas";
+import LogoAndSearchBar from "./LogoAndSearchBar";
+import Buttons from "./Buttons";
 
 const Navbar = () => {
   const { user, setUser } = useContext(UserContext);
@@ -29,49 +31,14 @@ const Navbar = () => {
 
   return (
     <div>
-      <div class="d-flex align-items-center">
-        <button
-          id="offcanvas_btn"
-          class="btn d-inline-block d-md-none navbar-toggler burguer-button"
-          type="button"
-          data-bs-toggle="offcanvas"
-          data-bs-target="#offcanvasExample"
-          aria-controls="offcanvasExample"
-        >
-          <span class="navbar-toggler-icon nav-span"></span>
-        </button>
-        <a class="navbar-brand" href="#" alt="logo">
-          <img src="/assets/icons/logo.png" alt="" />
-        </a>
-
-        <div
-          class="collapse navbar-collapse d-md-flex"
-          id="navbarSupportedContent"
-        >
-          <div class="input-wrapper">
-            <input
-              type="search"
-              class="input input-search"
-              id="find-input"
-              placeholder="Search..."
-            />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="input-icon"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </div>
-        </div>
+      <div>
+        <LogoAndSearchBar />
       </div>
       <div>
-        <NavbarComponents />
+        <Offcanvas />
+      </div>
+      <div>
+        <Buttons />
       </div>
     </div>
   );
--- src/components/Nav/NavbarComponents.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NavbarComponents = () => {
-  return <div>NavbarComponents</div>;
-};
-
-export default NavbarComponents;
