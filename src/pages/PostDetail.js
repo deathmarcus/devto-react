ac0913eff587e8b232806b2bcb7b3d1dd71d2183
araceli <araceli.romero@gmail.com>
2022-09-23 00:29:17 -0500

    creater right detail

--- src/pages/PostDetail.js
+++ src/pages/PostDetail.js
@@ -1,12 +1,13 @@
-import React from "react"
-import LeftAsideDetail from "../components/LeftAsideDetail"
+import React from "react";
+import RightAsideDetail from "../components/RightAsideDetail";
+//import LeftAsideDetail from "../components/LeftAsideDetail";
 
 const PostDetail = () => {
-    return(
-        <div>
-            <LeftAsideDetail />
-        </div>
-    )
-}
+  return (
+    <div>
+      <RightAsideDetail />
+    </div>
+  );
+};
 
 export default PostDetail;
