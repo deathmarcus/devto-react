f4d771fa4b9d37476ad9c493ef77b62345669f21
Jose De Anda <josdandar@gmail.com>
2022-09-22 17:35:11 -0700

    Added svg

---
+++ src/pages/PostDetail.js
@@ -0,0 +1,12 @@
+import React from "react"
+import LeftAsideDetail from "../components/LeftAsideDetail"
+
+const PostDetail = () => {
+    return(
+        <div>
+            <LeftAsideDetail />
+        </div>
+    )
+}
+
+export default PostDetail;
