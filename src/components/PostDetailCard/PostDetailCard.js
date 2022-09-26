298de61a45ca9e6e02b4d6eaf447234eb3ece20b
luised2301 <luised2301@gmail.com>
2022-09-26 13:32:46 -0500

    hotfix postDetailCard

--- src/components/PostDetailCard/PostDetailCard.js
+++ src/components/PostDetailCard/PostDetailCard.js
@@ -2,8 +2,6 @@ import React, { useState, useContext, useCallback } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import useFetch from "../../hooks/useFetchLike";
 
-const params = useParams();
-
 function PostDetailCard(postUrl) {
   const { data, loading, error } = useFetch(postUrl);
   if (data) console.log("POSTDATA:", { data });
